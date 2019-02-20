de48b9d65e2a1fb4737b00f82c69e261134d6a88
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-02-20 12:22:50 +0100

    Update Wavelets.py

--- Wavelets.py
+++ Wavelets.py
@@ -8,21 +8,23 @@ Created on Wed Feb 20 09:42:00 2019
 import numpy as np
 
 
-def lifting(s, j, n):
-    s_new = np.ones(2**(j))
+def lifting(s, j):
+    s_new = np.ones(2**(j-1))
     d_new = np.copy(s_new)
-    for k in range(2**(j)):
+    for k in range(2**(j-1)):
         s_new[k] = (s[2*k] + s[2*k + 1])/2
         d_new[k] = s_new[k] - s[2*k + 1]
     s = np.hstack((s_new, d_new))
     return s
+
+def liftingsteps():
+    
 
 
 j = 3
 
 sj = np.array([56, 40, 8, 24, 48, 48, 40, 16])
 
-sj_1 = lifting(sj, j-1)
 
 #sj_1, dj_1 = lifting(sj, j-1)
 #sj_2, dj_2 = lifting(sj_1, j-2)

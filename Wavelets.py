b5f5551ac02baf25b4157b023ad21dc87d37f6c9
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-02-20 11:53:21 +0100

    Update Wavelets.py

--- Wavelets.py
+++ Wavelets.py
@@ -7,13 +7,25 @@ Created on Wed Feb 20 09:42:00 2019
 
 import numpy as np
 
+
+def lifting(s, j, n):
+    s_new = np.ones(2**(j))
+    d_new = np.copy(s_new)
+    for k in range(2**(j)):
+        s_new[k] = (s[2*k] + s[2*k + 1])/2
+        d_new[k] = s_new[k] - s[2*k + 1]
+    s = np.hstack((s_new, d_new))
+    return s
+
+
 j = 3
 
 sj = np.array([56, 40, 8, 24, 48, 48, 40, 16])
 
+sj_1 = lifting(sj, j-1)
 
-sj_1 = np.ones(2**(j-1))
-for component in sj_1:
-    component = (sj[2*component] + sj[2*component + 1])/2
-
-dj_1 = 5
+#sj_1, dj_1 = lifting(sj, j-1)
+#sj_2, dj_2 = lifting(sj_1, j-2)
+#sj_3, dj_3 = lifting(sj_2, j-3)
+#
+#sj = np.hstack((sj_3, dj_3, dj_2,dj_1))

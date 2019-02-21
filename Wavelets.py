227f343db369255915dfeaf611d7a1ee842887b1
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-02-21 08:42:37 +0100

    added zeroPadding function

--- Wavelets.py
+++ Wavelets.py
@@ -8,7 +8,7 @@ Created on Wed Feb 20 09:42:00 2019
 import numpy as np
 
 
-def Wa(s, j, n):
+def Wa(s, n):
     for i in range(n):
         s = Ta(s, j - i)
     return s
@@ -19,11 +19,11 @@ def Ta(s, j):
         s[i + 1] = (s[i] + s[i + 1])/2
         s[i] = s[i] - s[i + 1]
     s[range(2**(j))] = s[list(range(1, 2**(j) + 1, 2)) +
-      list(range(0, 2**(j), 2))]
+                         list(range(0, 2**(j), 2))]
     return s
 
 
-def Ws(s, j, n):
+def Ws(s, n):
     for i in range(1, n + 1):
         s = Ts(s, i)
     return s
@@ -45,10 +45,16 @@ def update(d, k):
     return d[k]/2
 
 
+def zeroPadding(s):
+    if np.log2(len(s)) - int(np.log2(len(s))) != 0.0:
+        s = np.hstack((s, np.zeros(2**(int(np.log2(len(s))) + 1) - len(s))))
+    return s
+
 sj = np.array([56, 40, 8, 24, 48, 48, 40, 16])
+sj = zeroPadding(sj)
 j = int(np.log2(len(sj)))
-sj = Wa(sj, j, 3)
-sj = Ws(sj, j, 3)
+sj = Wa(sj, 3)
+sj = Ws(sj, 3)
 
 # =============================================================================
 # Tried to implement code from ``Ripples in Mathematics''

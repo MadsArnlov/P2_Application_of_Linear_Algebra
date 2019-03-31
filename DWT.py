029bd25750df9457056fa7beeb03385e3ea7dc24
Lasse Andersen <lasse2507a@gmail.com>
2019-03-31 16:53:14 +0200

    Update DWT.py

--- DWT.py
+++ DWT.py
@@ -66,10 +66,10 @@ def multiresolution(signal, filt, path = [0]):
             signal = cir_conv_downs(signal[1], filt)
             multires.append(signal)
     
+    plt.figure(figsize=(13, 7))
     for i in range(len(multires)):
         plt.subplot(len(multires), 2, i+2)
         plt.plot(multires[0], 'b.')
-    plt.figure(figsize=(13, 7))
     plt.show()
 
 # =============================================================================

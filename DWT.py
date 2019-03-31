2e350775aee629770cb74a446298a1326d4ba7cb
Lasse Andersen <lasse2507a@gmail.com>
2019-03-31 16:12:09 +0200

    Update DWT.py

--- DWT.py
+++ DWT.py
@@ -7,7 +7,7 @@ Created on Sat Mar 30 12:10:44 2019
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy as sc
+import scipy as sp
 from scipy import ndimage
 start = time.time()
 
@@ -44,8 +44,8 @@ def wave_generator(J = 10, freq = 10, phase = 0):
 # Convolution
 # =============================================================================
 def cir_conv_downs(signal, filt):
-    h = sc.ndimage.convolve1d(signal, filt[0], output = 'float', mode = 'wrap', origin = -1)
-    g = sc.ndimage.convolve1d(signal, filt[1], output = 'float', mode = 'wrap', origin = -1)
+    h = sp.ndimage.convolve1d(signal, filt[0], output = 'float', mode = 'wrap', origin = -1)
+    g = sp.ndimage.convolve1d(signal, filt[1], output = 'float', mode = 'wrap', origin = -1)
     h = h[0:len(h):2]
     g = g[0:len(g):2]
     return h, g

8f1722a50e345934b3bb62333b433223b46faf34
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-04-15 10:53:50 +0200

    Update csb.py

--- CSB/csb.py
+++ CSB/csb.py
@@ -9,6 +9,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def bisect(fcn, a, b, eps):
+    """
+    Bisection method for solution of the equation `fcn(x)=0` to an
+    accuracy `eps`, in the interval [a,b].
+    """
+    if fcn(a) * fcn(b) > 0:
+        print('Two endpoints have same sign')
+        return
+
+    count = 0
+    while abs(b - a) >= eps:
+        m = (a + b) / 2
+        if fcn(a) * fcn(m) <= 0:
+            b = m
+        else:
+            a = m
+        count += 1
+    return m, count
+
+
+def newton(f, df, x0, eps):
+    old = x0 + 1        # Ensure iteration starts
+    count = 0
+    while abs(x0-old) > eps:
+        old = x0
+        x0 = old - f(old)/df(old)
+        count += 1
+    return x0, count
+
+
+def secant(f, x0, x1, eps):
+    count = 0
+    while abs(x0 - x1) > eps:
+        x_temp = x1-(f(x1)*(x1-x0))/(f(x1)-f(x0))
+        x0 = x1
+        x1 = x_temp
+        count += 1
+    return x1, count
+
+
 def f(x, h0, lamb):
     return h0 + lamb*np.cosh(x/lamb)
 

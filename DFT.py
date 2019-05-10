17191a4a595241bfc668d9e3d255f00583c7d64e
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-05-10 14:00:11 +0200

    DFT phase information

--- DFT.py
+++ DFT.py
@@ -28,30 +28,34 @@ def fft(signal, fs = 1, highest_frequency = 1250):
     duration = N / fs
     spectrum = int(highest_frequency*duration)
     frequencies = np.arange(0, N // 2) / duration
-    x_fft = np.abs(np.fft.fft(signal, norm = 'ortho'))[0:N // 2]
+    x_fft = np.fft.fft(signal, norm = 'ortho')[0:N // 2]
+
     plt.figure(figsize=(12, 7))
     plt.subplot(2, 1, 1)
     plt.plot(signal, 'r,')
     plt.grid()
     plt.subplot(2, 1, 2)
-    plt.plot(frequencies[:spectrum], x_fft[:spectrum], 'b-')
+    plt.plot(frequencies[:spectrum], np.abs(x_fft)[:spectrum], 'b-')
     plt.grid()
     plt.show()
-    return x_fft, frequencies, spectrum
+    return x_fft, duration, spectrum, frequencies
 
 
-def new_freq(x_fft1, x_fft2, frequencies, spectrum):
-    x_fft3 = x_fft2 - x_fft1
+def new_freq(x_fft1, x_fft2, frequencies, spectrum, duration):
+    x_fft_difference = np.abs(x_fft2) - np.abs(x_fft1)
     plt.figure(figsize=(12, 3.5))
     plt.grid()
-    plt.plot(frequencies[:spectrum], x_fft3[:spectrum], 'b-')
+    plt.plot(frequencies[:spectrum], x_fft_difference[:spectrum], 'b-')
     plt.show()
 
-    new_signal = 5000 * np.sin((2 * np.pi * np.arange(0, 48000) / 48000) * np.argmax(x_fft3)/5.46)
+    t = (2 * np.pi * np.arange(0, 48000) / 48000)
+    phase = np.arctan2(np.real(x_fft2[np.argmax(x_fft_difference)]),
+                       np.imag(x_fft2[np.argmax(x_fft_difference)]))
+    new_signal = 5000 * np.sin(t * np.argmax(x_fft_difference) / duration + phase)
     plt.figure(figsize=(12, 3.5))
     plt.plot(range(0, spectrum), new_signal[:spectrum], 'k.')
     plt.show()
-    return new_signal, x_fft3
+    return new_signal, x_fft_difference
 
 
 # =============================================================================
@@ -93,8 +97,8 @@ def sample_delay(time1, time2, time3):
 # =============================================================================
 # Data
 # =============================================================================
-data_folder = Path("Test_recordings/Without_noise/1000-500Hz_speaker2_uden_støj/")
-file_to_open = [data_folder / "Test_recording microphone{:d}_1000-500Hz_speaker2_uden_støj.wav".format(i) for i in range(1,4)]
+data_folder = Path("Test_recordings/Without_noise/240-480Hz_speaker4_uden_støj/")
+file_to_open = [data_folder / "Test_recording microphone{:d}_240-480Hz_speaker4_uden_støj.wav".format(i) for i in range(1,4)]
 
 sampling_frequency, data1 = wavfile.read(file_to_open[0])
 sampling_frequency, data2 = wavfile.read(file_to_open[1])
@@ -112,12 +116,12 @@ x_fault = [data1[data_m2:data_e], data2[data_m2:data_e], data3[data_m2:data_e]]
 # =============================================================================
 # Execution
 # =============================================================================
-x_fft0_prior, frequencies, spectrum = fft(x_prior[0], sampling_frequency)
-x_fft0_fault, frequencies, spectrum = fft(x_fault[0], sampling_frequency)
-new_signal, x_fft_new = new_freq(x_fft0_prior, x_fft0_fault, frequencies, spectrum)
-fft(new_signal, sampling_frequency)
+x_fft0_prior, duration, spectrum, frequencies = fft(x_prior[0], sampling_frequency)
+x_fft0_fault, duration, spectrum, frequencies = fft(x_fault[0], sampling_frequency)
+new_signal, x_fft_new = new_freq(x_fft0_prior, x_fft0_fault, frequencies, spectrum, duration)
+#fft(new_signal, sampling_frequency)
 
-new_signal = new_signal[:12000]
+#new_signal = new_signal[:12000]
 
 time1 = cross_corr(new_signal, data1[1000000:], samples = 48000)
 time2 = cross_corr(new_signal, data2[1000000:], samples = 48000)

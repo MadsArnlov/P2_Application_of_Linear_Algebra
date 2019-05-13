26171ee9fe06840b71d5c26c149b66562fadb1d3
lasse2507a <44569984+lasse2507a@users.noreply.github.com>
2019-05-13 12:17:00 +0200

    ass

--- pywavelets_implementation.py
+++ pywavelets_implementation.py
@@ -40,6 +40,42 @@ x_1 = np.array([1,2,3,4,5,6,7,8])
 # =============================================================================
 # implementatering af pywt packet decomposition
 # =============================================================================
+def packets_selection(signal, packets, threshold, freq1, freq2, freq3, freq4):
+    path_list = []
+#    threshold = np.sum(signal**2)
+    for i in wavelet_packet.get_leaf_nodes(False):
+        if np.sum((wavelet_packet[i.path].data)**2) < threshold:
+            del wavelet_packet[i.path]
+        else:
+            path_list.append(i.path)
+
+        list_freq_spec = []
+    for i in range(len(path_list)):
+        freq_spec = [0, 48000/2]
+        for j in range(len(path_list[i])):
+            if  path_list[i][j] == 'b':
+                freq_spec_temp = freq_spec[0]
+                freq_spec[0] = freq_spec[1]   
+                freq_spec[1] = freq_spec_temp + (freq_spec[0] - freq_spec_temp)/2
+            elif  path_list[i][j] == 'a':
+                freq_spec[1] = freq_spec[1] + (freq_spec[0] - freq_spec[1])/2
+        list_freq_spec.append(freq_spec)
+    
+    remove_index_list = []
+    for i in range(len(path_list)):
+        if freq1 >= list_freq_spec[i][0] - 10 and freq1 <= list_freq_spec[i][1] + 10:
+            remove_index_list.append(i)
+        elif freq2 >= list_freq_spec[i][0] - 10 and freq2 <= list_freq_spec[i][1] + 10:
+            remove_index_list.append(i)
+        elif freq3 >= list_freq_spec[i][0] - 10 and freq3 <= list_freq_spec[i][1] + 10:
+            remove_index_list.append(i)
+        elif freq4 >= list_freq_spec[i][0] - 10 and freq4 <= list_freq_spec[i][1] + 10:
+            remove_index_list.append(i)
+    for i in remove_index_list:
+         del wavelet_packet[path_list[i]]
+         del list_freq_spec[i]
+    return wavelet_packet, list_freq_spec
+
 max_level = pywt.dwt_max_level(len(x_1), "haar")
 
 wavelet_packet = pywt.WaveletPacket(x_1,"haar")

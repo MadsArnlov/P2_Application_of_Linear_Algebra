037e6f49fb0e0833bfbf7923dadeb7896b445de1
mlade18 <mlade18@student.aau.dk>
2019-05-15 10:54:51 +0200

    Impuls virker
    Shit_Works

--- pywavelets_implementation.py
+++ pywavelets_implementation.py
@@ -25,15 +25,19 @@ def format_array(a):
 # =============================================================================
 # Import af data
 # =============================================================================
-data_folder = Path("C:\\Users\\bergl\\OneDrive\\Documents\\GitHub\\P2_Application_of_Linear_Algebra\\Test_recordings\\Without_noise\\impuls300pr.min_speaker2_uden_støj")
-file_to_open = [data_folder / "Test_recording microphone{:d}_impuls_speaker2_uden_støj.wav".format(i) for i in range(1,4)]
+data_folder = Path("C:\\Users\\bergl\\OneDrive\\Documents\\GitHub\\P2_Application_of_Linear_Algebra\\Test_recordings\\Without_noise\\impuls300pr.min_speaker3_uden_støj")
+file_to_open = [data_folder / "Test_recording microphone{:d}_impuls_speaker3_uden_støj.wav".format(i) for i in range(1,4)]
+#
+#data_folder = Path("C:\\Users\\bergl\\OneDrive\\Documents\\GitHub\\P2_Application_of_Linear_Algebra\\Test_recordings\\Without_noise\\737-368.5Hz_speaker3_uden_støj")
+#file_to_open = [data_folder / "Test_recording microphone{:d}_737-368.5Hz_speaker3_uden_støj.wav".format(i) for i in range(1,4)]
 
 sampling_frequency, data1 = wavfile.read(file_to_open[0])
 sampling_frequency, data2 = wavfile.read(file_to_open[1])
 sampling_frequency, data3 = wavfile.read(file_to_open[2])
 
-data_s = sampling_frequency * 21         # start value for data interval
-data_e = data_s + 2**19                  # end value for data interval
+#data_s = sampling_frequency * 21         # start value for data interval'
+data_s=800000
+data_e = data_s + 2**19                 # end value for data interval
 
 x = [data1[data_s:data_e], data2[data_s:data_e], data3[data_s:data_e]]
 x_1 = np.array([1,2,3,4,5,6,7,8])
@@ -117,7 +121,7 @@ def cross_corr(signal1, signal2):
 #print(wp["aaaaaaaa"].data)
 
 # =============================================================================
-def packets_selection(signal, level, threshold=0, freq1=737, freq2=240, freq3=1000, freq4=125, filters="db16"):
+def packets_selection(signal, level, safe_zone = 1000, threshold=0, freq1=737, freq2=240, freq3=1000, freq4=125, filters="db16"):
     
 #    max_level = pywt.dwt_max_level(len(signal), filters)
 
@@ -131,12 +135,13 @@ def packets_selection(signal, level, threshold=0, freq1=737, freq2=240, freq3=10
             level_decomposition[i].data = np.zeros(len(level_decomposition[i].data))
         else:
             path_list.append(level_decomposition[i].path)
-
-        list_freq_spec = []
+    
+    
+    list_freq_spec = []
     for i in range(len(path_list)):
         freq_spec = [0, 48000/2]
         for j in range(len(path_list[i])):
-            if  path_list[i][j] == 'b':
+            if  path_list[i][j] == 'd':
                 freq_spec_temp = freq_spec[0]
                 freq_spec[0] = freq_spec[1]   
                 freq_spec[1] = freq_spec_temp + (freq_spec[0] - freq_spec_temp)/2
@@ -144,15 +149,24 @@ def packets_selection(signal, level, threshold=0, freq1=737, freq2=240, freq3=10
                 freq_spec[1] = freq_spec[1] + (freq_spec[0] - freq_spec[1])/2
         list_freq_spec.append(freq_spec)
     
+    
     remove_index_list = []
     for i in range(len(path_list)):
-        if freq1 >= list_freq_spec[i][0] + 10  and freq1 <= list_freq_spec[i][1] + 10:
+        if freq1 >= list_freq_spec[i][0] - safe_zone  and freq1 <= list_freq_spec[i][1] + safe_zone:
             remove_index_list.append(i)
-        elif freq2 >= list_freq_spec[i][0] + 10  and freq2 <= list_freq_spec[i][1] + 10:
+        elif freq2 >= list_freq_spec[i][0] - safe_zone  and freq2 <= list_freq_spec[i][1] + safe_zone:
             remove_index_list.append(i)
-        elif freq3 >= list_freq_spec[i][0] + 10  and freq3 <= list_freq_spec[i][1] + 10:
+        elif freq3 >= list_freq_spec[i][0] - safe_zone and freq3 <= list_freq_spec[i][1] + safe_zone:
             remove_index_list.append(i)
-        elif freq4 >= list_freq_spec[i][0] + 10  and freq4 <= list_freq_spec[i][1] + 10:
+        elif freq4 >= list_freq_spec[i][0] - safe_zone   and freq4 <= list_freq_spec[i][1] + safe_zone:
+            remove_index_list.append(i)
+        elif freq1 <= list_freq_spec[i][0] + safe_zone  and freq1 >= list_freq_spec[i][1] - safe_zone:
+            remove_index_list.append(i)
+        elif freq2 <= list_freq_spec[i][0] + safe_zone  and freq2 >= list_freq_spec[i][1] - safe_zone:
+            remove_index_list.append(i)
+        elif freq3 <= list_freq_spec[i][0] + safe_zone  and freq3 >= list_freq_spec[i][1] - safe_zone:
+            remove_index_list.append(i)
+        elif freq4 <= list_freq_spec[i][0] + safe_zone  and freq4 >= list_freq_spec[i][1] - safe_zone:
             remove_index_list.append(i)
     for i in remove_index_list:
         level_decomposition[i].data = np.zeros(len(level_decomposition[i].data))    
@@ -161,14 +175,13 @@ def packets_selection(signal, level, threshold=0, freq1=737, freq2=240, freq3=10
     usefull_path_list=[]
     for i in level_decomposition:
         if i.data.any() != 0:
-            print(i.path, type(i.path))
+#            print(i.path, type(i.path))
             usefull_path_list.append(i.path)
             
     print(wp.reconstruct())
-    
-    
     synthesis = wp.reconstruct()
     return synthesis, usefull_path_list
+
 
 
 def reconstruct_from_packet(signal, level, path_list, filters="db16"):
@@ -187,6 +200,7 @@ def reconstruct_from_packet(signal, level, path_list, filters="db16"):
     return synthesis
 
         
+#def position_from_cc(time_delay_1_2 = position_1_2,time_delay_1_3 = position_1_3,time_delay_2_3 = position_2_3):
     
     
     
@@ -197,15 +211,30 @@ def reconstruct_from_packet(signal, level, path_list, filters="db16"):
 #x_h1 = np.array(hamming(x[1]))
 #x_h2 = np.array(hamming(x[2]))
 
+# =============================================================================
+# calling packets_selection and Reconstruct_from_packet
+# =============================================================================
+synthesis1, usefull_path_list = packets_selection(x[1], 10)
+synthesis2 = reconstruct_from_packet(x[0], 10, usefull_path_list)
+synthesis3 = reconstruct_from_packet(x[2], 10, usefull_path_list)
 
-#data_norm_by_std0 = x[0]/scipy.std(x[0])
-#data_norm_by_std1 = x[1]/scipy.std(x[1])
-#data_norm_by_std2 = x[2]/scipy.std(x[2])
-synthesis1, usefull_path_list = packets_selection(x[0], 8)
-synthesis2 = reconstruct_from_packet(x[1], 8, usefull_path_list)
-synthesis3 = reconstruct_from_packet(x[2], 8, usefull_path_list)
-cross_corr(synthesis2[300000:400000]/scipy.std(synthesis2), synthesis3[300000:400000]/scipy.std(synthesis3))
+#synthesis4 = reconstruct_from_packet(x[0], 6, ["dddddd"])
+#synthesis5 = reconstruct_from_packet(x[1], 6, ["dddddd"])
+#synthesis6 = reconstruct_from_packet(x[2], 6, ["dddddd"])
+# =============================================================================
+# calling Cross_corr
+# =============================================================================
+position_1_2=cross_corr(synthesis1[300000:400000]/scipy.std(synthesis1), synthesis2[300000:400000]/scipy.std(synthesis2))
+position_1_3=cross_corr(synthesis1[300000:400000]/scipy.std(synthesis1), synthesis3[300000:400000]/scipy.std(synthesis3))
+position_2_3=cross_corr(synthesis2[300000:400000]/scipy.std(synthesis2), synthesis3[300000:400000]/scipy.std(synthesis3))
 
+
+#position_1_2=cross_corr(synthesis4[300000:400000]/scipy.std(synthesis4), synthesis5[300000:400000]/scipy.std(synthesis5))
+#position_1_3=cross_corr(synthesis4[300000:400000]/scipy.std(synthesis4), synthesis6[300000:400000]/scipy.std(synthesis6))
+#position_2_3=cross_corr(synthesis5[300000:400000]/scipy.std(synthesis5), synthesis6[300000:400000]/scipy.std(synthesis6))
+# =============================================================================
+# plot 
+# =============================================================================
 plt.figure(figsize=(14,7))
 plt.subplot(4,1,1)
 plt.plot(x[0])
@@ -216,9 +245,12 @@ plt.plot(synthesis2[100000:400000]/scipy.std(synthesis2))
 plt.subplot(4,1,4)
 plt.plot(synthesis3[100000:400000]/scipy.std(synthesis3))
 
-a=np.array([0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,0,0,0,0,0,0,0,0,0,0,0,0,0])
-b=np.array([1,2,3,4,5,6,7,8,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
-cross_corr(b/scipy.std(b),a/scipy.std(a))
+
+#a=np.array([0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,0,0,0,0,0,0,0,0,0,0,0,0,0])
+#b=np.array([1,2,3,4,5,6,7,8,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+#cross_corr(b/scipy.std(b),a/scipy.std(a))
+
+
 
 
 

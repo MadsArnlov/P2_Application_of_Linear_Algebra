3608ed1b79dc43677367f77e298476bb3f13b5f8
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-05-17 23:11:03 +0200

    identified

--- DFT.py
+++ DFT.py
@@ -30,33 +30,38 @@ def fft(signal, fs = 1, highest_frequency = 1250):
     frequencies = np.arange(0, N // 2) / duration
     x_fft = np.fft.fft(signal, norm = 'ortho')[0:N // 2]
 
-#    t = (2 * np.pi * np.arange(0, fs) / fs)
-#    plt.figure(figsize=(12, 7))
-#    plt.subplot(2, 1, 1)
-#    plt.plot(signal, 'r,')
-#    plt.grid()
-#    plt.subplot(2, 1, 2)
-#    plt.plot(frequencies[:spectrum], np.abs(x_fft)[:spectrum], 'k-')
-#    plt.grid()
-#    plt.show()
+    t = (2 * np.pi * np.arange(0, fs) / fs)
+    plt.figure(figsize=(12, 7))
+    plt.subplot(2, 1, 1)
+    plt.plot(signal, 'r,')
+    plt.grid()
+    plt.subplot(2, 1, 2)
+    plt.plot(frequencies[:spectrum], np.abs(x_fft)[:spectrum], 'k-')
+    plt.grid()
+    plt.show()
     return x_fft, duration, spectrum, frequencies
 
 
 def new_freq(x_fft1, x_fft2, frequencies, spectrum, duration):
     x_fft_difference = np.abs(x_fft2) - np.abs(x_fft1)
-#    plt.figure(figsize=(12, 3.5))
-#    plt.grid()
-#    plt.plot(frequencies[:spectrum], x_fft_difference[:spectrum], 'k-')
-#    plt.show()
+    plt.figure(figsize=(12, 3.5))
+#    plt.subplot(2,1,1)
+    plt.grid()
+    plt.xlabel("Frequency [Hz]", fontsize=14)
+    plt.plot(frequencies[:spectrum], x_fft_difference[:spectrum], 'k-')
+    plt.show()
 
-    t = (2 * np.pi * np.arange(0, 48000) / 48000)
+    t = (np.arange(0, 48000) / 48000)
     phase = np.arctan2(np.real(x_fft2[np.argmax(x_fft_difference)]),
                        np.imag(x_fft2[np.argmax(x_fft_difference)]))
     frequency = np.argmax(x_fft_difference) / duration
-    new_signal = 5000 * np.sin(t * frequency + phase)
+    new_signal = np.sin(2*np.pi*t * frequency + phase)
     plt.figure(figsize=(12, 3.5))
-    plt.plot(new_signal[:spectrum], 'k-')
+#    plt.subplot(2,1,2)
+    plt.plot(t[:spectrum], new_signal[:spectrum], 'k-')
+    plt.xlabel("Time [s]", fontsize=14)
     plt.grid()
+#    plt.savefig("frequency_spectrum.pdf")
     plt.show()
     return new_signal, frequency
 
@@ -134,13 +139,13 @@ x_fft0_fault, duration, spectrum, frequencies = fft(x_fault[0], sampling_frequen
 new_signal, new_frequency = new_freq(x_fft0_prior, x_fft0_fault, frequencies, spectrum, duration)
 #fft(new_signal, sampling_frequency)
 
-time2 = cross_corr(new_signal[:int(sampling_frequency/2*new_frequency)], data1[sampling_frequency*15:sampling_frequency*20:])
-time1 = cross_corr(new_signal, data2[sampling_frequency*20:])
-time3 = cross_corr(new_signal, data3[sampling_frequency*20:])
-
-sample_delay(time1, time2, time3, new_frequency)
-
-end = time.time()
+#time2 = cross_corr(new_signal[:int(sampling_frequency/2*new_frequency)], data1[sampling_frequency*15:sampling_frequency*20:])
+#time1 = cross_corr(new_signal, data2[sampling_frequency*20:])
+#time3 = cross_corr(new_signal, data3[sampling_frequency*20:])
+#
+#sample_delay(time1, time2, time3, new_frequency)
+#
+#end = time.time()
 
 # =============================================================================
 # Synthetic: Identifying new frequency
@@ -149,28 +154,28 @@ end = time.time()
 #                   phase2 = 0, phase3 = np.pi/2, phase4 = np.pi/3)
 #signal2 = fsinew(J=18,freq1 = 0, freq2 = 0, freq3 = 10, freq4 = 50, phase1 = 0, 
 #                   phase2 = 0, phase3 = 0, phase4 = np.pi/3)
-#
-#x_fft0_prior, duration, spectrum, frequencies = fft(signal1, highest_frequency=75, fs=len(signal1))
-#x_fft0_fault, duration, spectrum, frequencies = fft(signal2, highest_frequency=75, fs=len(signal2))
+#noise = np.random.normal(0,0.5, 2**18)
+#x_fft0_prior, duration, spectrum, frequencies = fft(signal1+noise, highest_frequency=75, fs=len(signal1))
+#x_fft0_fault, duration, spectrum, frequencies = fft(signal2+noise, highest_frequency=75, fs=len(signal2))
 #new_signal, new_frequency = new_freq(x_fft0_prior, x_fft0_fault, frequencies, spectrum, duration)
-#
-#
+
+
 #t = (1 * np.arange(0, len(signal1)) / len(signal2))
 #plt.figure(figsize=(12, 10))
 #plt.subplot(2, 2, 1)
-#plt.plot(t, signal1, 'r-')
+#plt.plot(t, signal1+noise, 'r--')
 #plt.grid()
 #plt.xlabel("Time [s]", fontsize=14)
 #plt.subplot(2, 2, 2)
-#plt.plot(t, signal2, 'b-')
+#plt.plot(t, signal2+noise, 'b--')
 #plt.grid()
 #plt.xlabel("Time [s]", fontsize=14)
 #plt.subplot(2,2,3)
-#plt.stem(frequencies[:spectrum], np.abs(x_fft0_prior)[:spectrum], 'k-', 'k.')
+#plt.stem(frequencies[:spectrum], np.abs(x_fft0_prior)[:spectrum], 'k-', 'ko')
 #plt.grid()
 #plt.xlabel("Frequency [Hz]", fontsize=14)
 #plt.subplot(2,2,4)
-#plt.stem(frequencies[:spectrum], np.abs(x_fft0_fault)[:spectrum], 'k-', 'k.')
+#plt.stem(frequencies[:spectrum], np.abs(x_fft0_fault)[:spectrum], 'k-', 'ko')
 #plt.grid()
 #plt.xlabel("Frequency [Hz]", fontsize=14)
 #plt.savefig("identify_new_frequency.pdf")

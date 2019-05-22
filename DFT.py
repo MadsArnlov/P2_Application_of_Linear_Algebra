c412101140707e2cb85869d2e2356ad424939751
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-05-22 16:30:39 +0200

    Update DFT.py

--- DFT.py
+++ DFT.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from pathlib import Path
 from data_manipulation import zpad, hann, hamming, recw, fsinew, sinew
-#import acoustics
+import acoustics
 #start = time.time()
 
 
@@ -51,7 +51,7 @@ def new_freq(x_fft1, x_fft2, frequencies, spectrum, duration):
     plt.xlabel("Frequency [Hz]", fontsize=14)
     plt.plot(frequencies[:spectrum], x_fft_difference[:spectrum], 'k-')
     plt.show()
-    i = np.argmax(x_fft_difference)
+    i = np.argmax(x_fft_difference[:spectrum])
     X = np.zeros(len(x_fft1), dtype=complex)
     X[i] = x_fft2[i]
     X[-i] = x_fft2[-i]
@@ -59,7 +59,7 @@ def new_freq(x_fft1, x_fft2, frequencies, spectrum, duration):
 #    t = (np.arange(0, 48000*30) / 48000)
 #    phase = np.arctan2(np.real(x_fft2[np.argmax(x_fft_difference)]),
 #                       np.imag(x_fft2[np.argmax(x_fft_difference)]))
-    frequency = np.argmax(x_fft_difference[:spectrum]) / duration
+    frequency = i / duration
     frequency = int(frequency*10)*10**-1
 #    new_signal = np.sin(t * 2*np.pi*frequency + phase)
     plt.figure(figsize=(12, 3.5))
@@ -121,8 +121,8 @@ def sample_delay(time1, time2, time3, frequency):
 # =============================================================================
 # Data
 # =============================================================================
-data_folder = Path("Test_recordings/With_noise/737-368.5Hz_speaker3/")
-file_to_open = [data_folder / "Test_recording microphone{}_737-368.5Hz_speaker3.wav".format(i) for i in range(1,4)]
+data_folder = Path("Test_recordings/With_noise/240-480Hz_speaker4/")
+file_to_open = [data_folder / "Test_recording microphone{}_240-480Hz_speaker4.wav".format(i) for i in range(1,4)]
 
 sampling_frequency, data1 = wavfile.read(file_to_open[1])
 sampling_frequency, data2 = wavfile.read(file_to_open[0])
@@ -177,35 +177,29 @@ print(new_frequency0, new_frequency1, new_frequency2)
 delay = sample_delay(sample1, sample2, sample3, new_frequency0)
 
 print(56*"-", "\n", delay[0], delay[1], delay[2])
-#time2 = cross_corr(new_signal0[:len(new_signal0)//3], data1[sampling_frequency*18:sampling_frequency*18 + int(duration*sampling_frequency)//2])
-#time1 = cross_corr(new_signal0[:len(new_signal0)//3], data2[sampling_frequency*18:sampling_frequency*18 + int(duration*sampling_frequency)//2])
-#time3 = cross_corr(new_signal0[:len(new_signal0)//3], data3[sampling_frequency*18:sampling_frequency*18 + int(duration*sampling_frequency)//2])
-#
-#sample_delay(time1, time2, time3, new_frequency0)
-
-#end = time.time()
-
 # =============================================================================
 # Synthetic: Identifying new frequency
 # =============================================================================
 #signal1 = fsinew(J=18,freq1 = 0, freq2 = 0, freq3 = 0, freq4 = 10, phase1 = 0, 
 #                   phase2 = 0, phase3 = 0, phase4 = 0)
 #signal2 = fsinew(J=18,freq1 = 0, freq2 = 0, freq3 = 0, freq4 = 10, phase1 = 0, 
-#                   phase2 = 0, phase3 = 0, phase4 = 2*np.pi*2/4)
+#                   phase2 = 0, phase3 = 0, phase4 = 2*np.pi*1/4)
 #noise1 = acoustics.generator.brown(2**18)
 #noise2 = acoustics.generator.brown(2**18)
-#signal1 = signal1 + noise1
-#signal2 = signal2 + noise2
+#signal1 = signal1 + noise1/4
+#signal2 = signal2 + noise2/4
 #signal1_dft = np.fft.fft(signal1)
 #X1 = np.zeros(len(signal1_dft), dtype=complex)
-#X1[np.argmax(np.abs(signal1_dft))] = signal1_dft[np.argmax(np.abs(signal1_dft))]
-#X1[-np.argmax(np.abs(signal1_dft))] = signal1_dft[-np.argmax(np.abs(signal1_dft))]
+#i = np.argmax(np.abs(signal1_dft))
+#X1[i] = signal1_dft[i]
+#X1[-i] = signal1_dft[-i]
 #new_signal1 = np.fft.ifft(X1)
 #
 #signal2_dft = np.fft.fft(signal2)
 #X2 = np.zeros(len(signal2_dft), dtype=complex)
-#X2[np.argmax(np.abs(signal2_dft))] = signal2_dft[np.argmax(np.abs(signal2_dft))]
-#X2[-np.argmax(np.abs(signal2_dft))] = signal2_dft[-np.argmax(np.abs(signal2_dft))]
+#i = np.argmax(np.abs(signal2_dft))
+#X2[i] = signal2_dft[i]
+#X2[-i] = signal2_dft[-i]
 #new_signal2 = np.fft.ifft(X2)
 #
 #period = int((2**18)/10)
@@ -221,13 +215,13 @@ print(56*"-", "\n", delay[0], delay[1], delay[2])
 #
 #
 #samples = cross_corr(new_signal1[:10*period], new_signal2[:9*period])
-
+#
 #period_samples = 2**18 / 10
 #
 #samples = samples % period_samples
 #if samples >= 2**18/10:
 #    samples = samples - period_samples
-
+#
 #print(samples)
 
 #x_fft0_prior, duration, spectrum, frequencies = fft(signal1+noise1/4, len(signal1), 70)

9e7a4f7be4a7b0b0a000158fda9fdb9174596444
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2019-05-24 18:49:21 +0200

    fft and photos

--- FFT.py
+++ FFT.py
@@ -10,6 +10,22 @@ from data_manipulation import zpad, hann, hamming, recw, fsinew, sinew
 # FFT
 # =============================================================================
 def identify_f(x1, x2):
+    """
+    Two signals x1 and x2 are compared in the frequency domain to determine
+    if x2 contains a new frequency.
+
+    X_diff is used to identify the frequency by the difference of modulus of
+    X1 and X2.
+
+    X_diff is then used to find the index of the new frequency.
+
+    All other indices than that of the new frequency, the mirrored frequency
+    and 0-frequency are set to 0 in X_identity.
+
+    Lastly the frequency and phase are calculated.
+    The phase is computed by using the duration of the signals x1 and x2,
+    given by len(x1)/fs.
+    """
     X1 = np.fft.fft(x1)
     X2 = np.fft.fft(x2)
     X_diff = np.abs(X2) - np.abs(X1)
@@ -18,6 +34,7 @@ def identify_f(x1, x2):
     X_identity = np.zeros(len(X_diff), dtype=complex)
     X_identity[index] = X2[index]
     X_identity[-index] = X2[-index]
+    X_identity[0] = X2[0]
     X = [X1, X2, X_identity]
 
     frequency = index / duration
@@ -27,9 +44,9 @@ def identify_f(x1, x2):
 
 def sample_delay(samples, frequency):
     delay = []
-    delay.append(samples[0] - samples[1])
-    delay.append(samples[0] - samples[2])
-    delay.append(samples[1] - samples[2])
+    delay.append(samples[1] - samples[0])
+    delay.append(samples[2] - samples[0])
+    delay.append(samples[2] - samples[1])
     period_samples = fs/frequency
     for i in range(len(delay)):
         if delay[i] < 0:
@@ -41,6 +58,10 @@ def sample_delay(samples, frequency):
     return delay
 
 
+def corr(x1, x2):
+    correlation = np.correlate(x1, x2, 'full')
+    d = len(x1) - (np.argmax(correlation) + 1)
+    return d
 # =============================================================================
 # Data
 # =============================================================================
@@ -58,9 +79,9 @@ data1, data2, data3 = data1/sp.std(data1), data2/sp.std(data2), data3/sp.std(dat
 f_new = 500
 
 data_s = fs * 0
-data_m1 = int(fs/f_new *1000)#data_s + fs*14
-data_e = int(fs*16 + fs/f_new*1000)#fs* 16
+data_m1 = int(fs/f_new *1000)
 data_m2 = fs*16
+data_e = int(data_m2 + fs/f_new*1000)
 
 x_prior = [data1[data_s:data_m1], data2[data_s:data_m1], data3[data_s:data_m1]]
 x_fault = [data1[data_m2:data_e], data2[data_m2:data_e], data3[data_m2:data_e]]
@@ -68,35 +89,52 @@ x_fault = [data1[data_m2:data_e], data2[data_m2:data_e], data3[data_m2:data_e]]
 # Execution
 # =============================================================================
 "Artificial x-axis for amplitude spectrum"
-#N = len(x_fault[0])
-#duration = N / fs
-#fmax = 1250
-#upper_limit = int(fmax*duration)
-#xfrequencies = np.arange(0, N // 2) / duration
+N = len(x_fault[0])
+duration = N / fs
+fmax = 1250
+upper_limit = int(fmax*duration)
+xfrequencies = np.arange(0, N // 2) / duration
 
 phase0, f0, X0 = identify_f(x_prior[0], x_fault[0])
 phase1, f1, X1= identify_f(x_prior[1], x_fault[1])
 phase2, f2, X2 = identify_f(x_prior[2], x_fault[2])
 
+x0 = np.fft.ifft(X0[2]).real
+x1 = np.fft.ifft(X1[2]).real
+x2 = np.fft.ifft(X2[2]).real
+
 f = (f0 + f1 + f2)/3
 
 w = 2*np.pi*f
+
+t = np.arange(0, N) / fs
+
+s10 = corr(x1[:10*int(fs/f)], x0[:9*int(fs/f)])
+s20 = corr(x2[:10*int(fs/f)], x0[:9*int(fs/f)])
+s21 = corr(x2[:10*int(fs/f)], x1[:9*int(fs/f)])
+
+s = [s10, s20, s21]
 
 samples = [phase0/w * fs, phase1/w * fs, phase2/w * fs]
 delay = sample_delay(samples, f)
 
 plt.figure(figsize=(16,9))
 plt.subplot(3,1,1)
-plt.plot(np.fft.ifft(X0[2])[:int(fs/f)], 'r-')
+plt.plot(x1[:10*int(fs/f)], 'r-', x0[:9*int(fs/f)], 'b-')
 plt.grid()
+plt.legend(())
 plt.subplot(3,1,2)
-plt.plot(np.fft.ifft(X1[2])[:int(fs/f)], 'r-')
+plt.plot(x2[:10*int(fs/f)], 'r-', x0[:9*int(fs/f)], 'b-')
 plt.grid()
+plt.legend(())
 plt.subplot(3,1,3)
-plt.plot(np.fft.ifft(X2[2])[:int(fs/f)], 'r-')
+plt.plot(x2[:10*int(fs/f)], 'r-', x1[:9*int(fs/f)], 'b-')
 plt.grid()
+plt.legend(())
+#plt.savefig("idft.png")
 plt.show()
 
 for i in range(len(delay)):
     print("The sample delay is", "{:4d}".format(int(delay[i])), "samples.")
 
+print(s)
